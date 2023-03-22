cbf6e27d02d3a625d0195949823b7522abff8c84
azufb <curiousazunas@gmail.com>
2023-03-22 20:50:32 +0900

    CORSについて

--- routes/api.js
+++ routes/api.js
@@ -5,6 +5,29 @@ const express = require('express');
 // ルーティング用のオブジェクトを作る
 const router = express.Router();
 
+// アクセスを許可するオリジンのリスト
+const allowList = ['http://localhost:3000', 'http://site.example:3000'];
+
+// CORSヘッダを追加してクロスオリジンからのリクエストを許可
+// useメソッドを使った記述した処理は、/apiへリクエストを送信すると必ず実行される
+router.use((req, res, next) => {
+  // アクセスを許可するオリジンのリストとOriginヘッダを比較し、Originヘッダが存在する/リストに存在するかを確認
+  if (req.headers.origin && allowList.includes(req.headers.origin)) {
+    // "*"は、どんなオリジンでもOKで、制限なし
+    // 第2引数の許可するオリジンは複数指定できない
+    res.header('Access-Control-Allow-Origin', req.headers.origin);
+  }
+  /* 
+  X-Tokenヘッダの送信を許可するために、
+  プリフライトリクエスト(単純リクエスト以外)が送信された場合のみ、
+  Access-Control-Allow-Originヘッダを追加する
+  */
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Headers', 'X-Token');
+  }
+  next();
+});
+
 // GETメソッドでリクエストを受け取った場合の処理
 router.get('/', (req, res) => {
   // HTTPヘッダのレスポンスに追加する
