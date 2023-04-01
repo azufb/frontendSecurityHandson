066fac733a6ac240d67d62ab0fecc60abca795a9
azufb <curiousazunas@gmail.com>
2023-04-01 21:01:35 +0900

    ルーティング

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
+const crypto = require('crypto');
+
 // express読み込み
 const express = require('express');
 // express初期化
 const app = express();
 // ポート指定
 const port = 3000;
+
+// EJSをテンプレートエンジンとして利用できるように設定
+app.set('view engine', 'ejs');
 
 // ルーティング用オブジェクト読み込み
 const api = require('./routes/api');
@@ -19,6 +24,23 @@ app.use('/api', api);
 // サーバへGETメソッドでリクエストがあった場合の処理
 app.get('/', (req, res, next) => {
   res.send('Top Page');
+});
+
+app.get('/csp', (req, res) => {
+  // リクエストのたびにランダムな文字列を生成（nonce値）
+  const nonceValue = crypto.randomBytes(16).toString('base64');
+  // CSPヘッダの値に、nonce値を渡す
+  // 動的なJavaScript読み込みをできるように、strict-dynamicキーワードも付与
+  res.header(
+    'Content-Security-Policy',
+    `script-src 'nonce-${nonceValue}' 'strict-dynamic';` +
+      "object-src 'none';" +
+      "base-uri 'none';" +
+      "require-trusted-types-for 'script';"
+  );
+
+  // HTMLにnonce値を渡すため、第2引数に入れる
+  res.render('csp', { nonce: nonceValue });
 });
 
 // サーバ起動
